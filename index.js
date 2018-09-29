a9cae9c8eea09bdb0b9beb8f69dd2996e3f68082
Adrian Adames <adrianadames@gmail.com>
2018-09-29 14:57:55 -0400

    Added a default endpoint '/' that verifies that the server is responding to requests.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { server } = require('./server');
 
 const port = process.env.PORT || 5000;
+
+server.use('/', (req,res) => res.send('Server up and running!'))
 server.listen(port, () => {
   console.log(`\n=== Server listening on port ${port}\n`);
 });
