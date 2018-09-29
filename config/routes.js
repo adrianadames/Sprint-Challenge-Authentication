76b5d3240034a406dcc8c67e711e99a5984326a3
Adrian Adames <adrianadames@gmail.com>
2018-09-29 14:49:32 -0400

    Finished answering the self-study questions.

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,10 @@
 const axios = require('axios');
 
+const knex = require('knex');
+const knexConfig = require('../knexfile');
+
+const db = knex(knexConfig.development);
+
 const { authenticate } = require('./middlewares');
 
 module.exports = server => {
@@ -15,6 +20,18 @@ function register(req, res) {
   //hash password
   const hash = bcrypt.hashSync(user.password, 10); //10 rounds for development speed (i.e. in the interest of time)
   user.password = hash;
+
+    db('users')
+      .insert(user)
+      .then(ids => {
+        db('users')
+          .where({id:ids[0]})
+          .first()
+          .then(user => {
+            req.status(200).json(user);
+          })
+          .catch(err => res.status(500).json({err}));
+      })
 
 }
 
